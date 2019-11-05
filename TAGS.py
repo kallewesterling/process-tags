98e4d7bc490e9a89b64625cea4b31bdc35a3c564
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2019-11-04 21:55:56 -0500

    working

--- TAGS.py
+++ TAGS.py
@@ -74,9 +74,15 @@ class Tweet():
         self.possibly_sensitive = self._json.get('possibly_sensitive', None)
         self.lang = self._json.get('lang', None)
         self.json_source = self._json.get('json_source', None)
-                  
+
         if isinstance(self.user, dict): self.user = self.user['id_str']
         self.user = User(self.user)
+
+        self.retweet = self.is_retweet()
+
+
+    def is_retweet(self):
+      return("retweeted_status" in self._json or self.full_text.lower()[0:2] == "rt")
 
     def error_handle(self):
         if 'error' in self._json:
@@ -90,10 +96,10 @@ class Tweet():
 class User():
     def __init__(self, id_str):
         self.cache_dir = user_cache_dir
-        
+
         self.id_str = str(id_str)
         self._json = get_json_from_cache(self.id_str, self.cache_dir)
-        
+
         self.contributors_enabled = self._json.get('contributors_enabled', None)
         self.created_at = self._json.get('created_at', None)
         self.description = self._json.get('description', None)
@@ -117,27 +123,48 @@ class User():
         self.url = self._json.get('url', None)
         self.utc_offset = self._json.get('utc_offset', None)
         self.verified = self._json.get('verified', None)
-                  
+
+
 class TweetSet():
-    def __init__(self, ids=[], suppress_warnings=False, progressbar=True, filter_key=None, filter_value=None):
+    def __init__(self, ids=[], suppress_warnings=False, progressbar=True, filter_key=None, filter_value=None, include_retweets=True):
         self.suppress_warnings = suppress_warnings
         self.ids = ids
         self.progressbar = progressbar
         self.tweets = []
-        
+        self.filter_key = filter_key
+        self.filter_value = filter_value.lower()
+
         if self.progressbar: bar = pb.ProgressBar(maxval=len(self.ids)).start()
+
         for i, id in enumerate(ids):
+            f = None
+
             if self.progressbar: bar.update(i)
+
             tweet = Tweet(id, suppress_warnings=self.suppress_warnings)
-            if filter_key and filter_value:
-              if tweet._json.get(filter_key, None).lower == filter_value: self.tweets.append(tweet)
-            else: self.tweets.append(tweet)
-            
+
+            if filter_key and filter_value: f = Filter(tweet, filter_key, filter_value)
+
+            if tweet.retweet and include_retweets and not f.filter_out:
+              self.tweets.append(tweet) # we want retweets and this tweet has not been filtered so it's good to go
+            elif tweet.retweet and include_retweets and f.filter_out:
+              pass # we want retweets but this tweet has already been filtered
+            elif not tweet.retweet and not f.filter_out:
+              self.tweets.append(tweet) # this is an original tweet so we want to do this.
+            elif not tweet.retweet and f.filter_out:
+              pass # we want to capture it but it has been filtered out already
+            elif tweet.retweet and not include_retweets:
+              pass # we don't want retweets, no matter whether they've been filtered or not
+            else:
+              print("tweet.retweet: ", tweet.retweet, "\ninclude_retweets", include_retweets, "\nf.filter_out", f.filter_out)
         if self.progressbar: bar.finish()
-            
+
     def __repr__(self):
         return(f"TweetSet consisting of {len(self.tweets)} tweets.")
-    
+
+    def __len__(self):
+      return(len(self.tweets))
+
 
 def get_json_from_cache(id_str, cache_dir):
     tweet_cache = cache_dir / id_str
@@ -156,3 +183,21 @@ def _expand_contractions(text, c_re=None):
     def replace(match):
         return contractions[match.group(0)]
     return(c_re.sub(replace, text))
+
+
+class Filter():
+
+  def __init__(self, tweet = None, filter_key = None, filter_value = None):
+    self.tweet = tweet
+    self.filter_key = filter_key
+    self.filter_value = filter_value.lower()
+
+    test_value = tweet._json.get(self.filter_key, "").lower()
+    if self.filter_value in test_value:
+      self.filter_in = True
+      self.filter_out = False
+    elif self.filter_value not in test_value:
+      self.filter_in = False
+      self.filter_out = True
+    else:
+      raise RuntimeError("Something strange happened here.")
